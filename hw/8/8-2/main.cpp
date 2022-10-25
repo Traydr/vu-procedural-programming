a78d64fcb51843071fd306a8e8251d037bbe0768
Traydr <Traydr@protonmail.com>
2022-10-25 10:53:20 +0300

    Complete hw8

--- hw/8/8-2/main.cpp
+++ hw/8/8-2/main.cpp
@@ -15,8 +15,15 @@ int main() {
   do {
     std::string input;
     std::cout << "Enter any word\n";
+
+    // Clear cin before getline since getline() and >> operators handle input differently (trailing \n chars and whitespace)
+    std::cin.clear();
+    std::cin.sync();
+    // Cleared and should be reset
+
     std::getline(std::cin, input);
-    int randomIndex = rand()%input.length();
+    int inputLength = input.length() > 0 ? input.length() : 1;
+    int randomIndex = rand()%inputLength;
 
     int counter{0};
     for (char c : input) {
