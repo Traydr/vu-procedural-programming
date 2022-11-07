2cd08c62b259bffce2cc579ddc53f3357677ad19
Traydr <Traydr@protonmail.com>
2022-11-07 09:07:41 +0200

    P2 impossible problem

---
+++ practicals/p2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+// Problem:
+// 8 islands
+// 5 bridges between the town and the islands
+// 4 islands have 4 bridges
+// 3 islands have 3 bridges
+// 1 islands has 1 bridge
+
+int main()
+{
+	// 0 - Town, 1 to 8 islands
+	bool connections[9][9]{};
+	return 0;
+}
+
+// Impossible since you are always missing 1 connections
